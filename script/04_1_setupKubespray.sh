c1513845ae4ef7182ad3cf83d2e6ea7a6adac8e5
hojat-gazestani <Hojat.Gazestani@gmail.com>
2023-07-05 12:50:58 +0330

    Added kubespray

--- script/04_1_setupKubespray.sh
+++ script/04_1_setupKubespray.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Define ANSI escape sequences for setting background and font colors
+GREEN_BACKGROUND='\033[42m'
+BLACK_FONT='\033[30m'
+DEFAULT_COLOR='\033[0m'
+
 setupKubespray() {
-  echo "Setting up Kubespray 1.3.0 ..."
+  echo "${GREEN_BACKGROUND}${BLACK_FONT}Setting up Kubespray 1.3.0 ...${DEFAULT_COLOR}"
   # I have tested Kubespray versions 1.3.0 and 2/16. By default, it will bootstrap with version 1.3.0.
   git clone https://github.com/kubernetes-sigs/kubespray.git || {
-    echo "Error: Failed to clone Kubespray repository." >&2
+    echo "${GREEN_BACKGROUND}${BLACK_FONT}Error: Failed to clone Kubespray repository.${DEFAULT_COLOR}" >&2
     exit 1
   }
   cd kubespray
@@ -13,7 +18,7 @@ setupKubespray() {
   #   exit 1
   # }
   git checkout 1.3.0 | {
-     echo "Error: Failed to switch to release-1.3.0 branch." >&2
+     echo "${GREEN_BACKGROUND}${BLACK_FONT}Error: Failed to switch to release-1.3.0 branch.${DEFAULT_COLOR}" >&2
      exit 1
   }
   sudo apt-get update -y
@@ -21,7 +26,7 @@ setupKubespray() {
   sudo apt install python3-venv python3-pip -y # python3.8-venv
   # Create Python virtual environment
   python3 -m venv venv || {
-    echo "Error: Failed to Create Python virtual environment." >&2
+    echo "${GREEN_BACKGROUND}${BLACK_FONT}Error: Failed to Create Python virtual environment.${DEFAULT_COLOR}" >&2
     exit 1
   }
   source venv/bin/activate
