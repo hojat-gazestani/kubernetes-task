b61c69970fd34a26b95af23dda1b40fb539fb583
hojat-gazestani <Hojat.Gazestani@gmail.com>
2023-07-06 16:41:32 +0330

    Added kubespray

--- script/04_1_setupKubespray.sh
+++ script/04_1_setupKubespray.sh
@@ -8,18 +8,19 @@ DEFAULT_COLOR='\033[0m'
 setupKubespray() {
   echo "${GREEN_BACKGROUND}${BLACK_FONT}Setting up Kubespray 1.3.0 ...${DEFAULT_COLOR}"
 
-  git clone https://github.com/kubernetes-sigs/kubespray.git || {
+  #git clone https://github.com/kubernetes-sigs/kubespray.git || {
+  git clone https://github.com/kubernetes-incubator/kubespray.git || {
     echo "${GREEN_BACKGROUND}${BLACK_FONT}Error: Failed to clone Kubespray repository.${DEFAULT_COLOR}" >&2
     exit 1
   }
   cd kubespray
-   git checkout release-2.16 || {
-     echo "Error: Failed to switch to release-2.16 branch." >&2
-     exit 1
-   }
+#  git checkout release-2.16 || {
+#    echo "Error: Failed to switch to release-2.16 branch." >&2
+#    exit 1
+#  }
   sudo apt-get update -y
   sudo apt-get update --fix-missing
-  sudo apt install python3-venv python3-pip -y # python3.8-venv
+  sudo apt install python3-venv python3-pip python3 -y
   # Create Python virtual environment
   python3 -m venv venv || {
     echo "${GREEN_BACKGROUND}${BLACK_FONT}Error: Failed to Create Python virtual environment.${DEFAULT_COLOR}" >&2
