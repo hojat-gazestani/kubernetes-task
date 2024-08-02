7cdc5cc044e18e4bd6a0c78ffe9b2c653965ad0e
hojat-gazestani <hojatgazestani@gmail.com>
2024-08-03 02:53:18 +0330

    minor fix

--- script/02_check_os.sh
+++ script/02_check_os.sh
@@ -10,8 +10,8 @@ check_os() {
     if [[ $(lsb_release -rs) == "20.04" ]]; then
         echo -e "${success_msg}Detected OS: $detected_os${reset}"
     else
-        echo -e "${error_msg}Hojat tested me on Ubuntu 20.04 and 22.04, And your OS is $detected_os. Would you like to continue? (y/n):${reset}"
-        read answer
+        echo -e "${error_msg}Hojat tested me on Ubuntu 20.04 and 22.04, And your OS is $detected_os. Would you like to continue? (Y/n):${reset}"
+        read -r answer
         if [[ ! $answer =~ ^[Yy]$|^yes$ ]]; then
             exit 1
         fi
--- script/04_kubespray_init.sh
+++ script/04_kubespray_init.sh
@@ -13,15 +13,15 @@ initialize_kubespray() {
   NODE=$(ip -o -4 addr show up scope global | awk '{print $4}' | cut -d "/" -f 1 | while read -r IP; do if ip route get 172.16.6.1 | grep -q "$IP"; then echo "$IP"; break; fi; done)
   
   echo -n -e "${warning_msg}Is $NODE your Kubernetes master node IP address (Y|n)?${reset}"
-  read -n 15 ANS
+  read -r -n 15 ANS
   if [[ $ANS =~ ^[Nn]$ ]] ; then
     echo -n  -e "${error_msg}Please enter the correct address:${reset}"
-    read -n 15 NODE
+    read -r -n 15 NODE
   fi
     
   echo -e "${success_msg}Setup a Kubernetes cluster on $NODE using Kubespray...${reset}"
 
-  declare -r CLUSTER_FOLDER='my-cluster'
+  export CLUSTER_FOLDER='my-cluster'
   setupKubespray
   configureKubespray
   runKubespray
