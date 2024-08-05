a1d3af1902b3302c0dc7e3a7cf40dcdcff3a0141
hojat-gazestani <hojatgazestani@gmail.com>
2024-08-06 01:39:25 +0330

    Configuration error handling

--- script/06_kubespray_configure.sh
+++ script/06_kubespray_configure.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+source ./script/00_messages.sh
+
 configureKubespray() {
-  echo "Configuring Kubespray..."
+  warning_message "Configuring Kubespray..."
 
-  # Generate inventory
+  warning_message "Generate inventory"
   declare -a IPS=($NODE)
-  CONFIG_FILE=inventory/$CLUSTER_FOLDER/hosts.yaml python3 contrib/inventory_builder/inventory.py "${IPS[@]}"
+  CONFIG_FILE=inventory/$CLUSTER_FOLDER/hosts.yaml python3 contrib/inventory_builder/inventory.py "${IPS[@]}" || error_message "Failed to generate inventory."
 
-  # Configure kubespray settings
-  sed -i 's/kube_proxy_strict_arp: false/kube_proxy_strict_arp: true/' inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/k8s-cluster.yml
-  sed -i 's/container_manager: docker/container_manager: containerd/' inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/k8s-cluster.yml
+  warning_message "Configure kubespray settings"
+  sed -i 's/kube_proxy_strict_arp: false/kube_proxy_strict_arp: true/' inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/k8s-cluster.yml || error_message "Failed to configure kube-proxy."
+  sed -i 's/container_manager: docker/container_manager: containerd/' inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/k8s-cluster.yml || error_message "Failed to configure container manager."
 
-  sed -i 's/metallb_enabled: false/metallb_enabled: true/' inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/addons.yml
+  sed -i 's/metallb_enabled: false/metallb_enabled: true/' inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/addons.yml || error_message "Failed to enable MetalLB."
 
   cat >> inventory/$CLUSTER_FOLDER/group_vars/k8s_cluster/addons.yml << EOF
 metallb_ip_range:
@@ -27,7 +29,7 @@ metallb_controller_tolerations:
     effect: "NoSchedule"
 EOF
 
-  sed -i 's|etcd_deployment_type: docker|etcd_deployment_type: host|' inventory/$CLUSTER_FOLDER/group_vars/etcd.yml
+  sed -i 's|etcd_deployment_type: docker|etcd_deployment_type: host|' inventory/$CLUSTER_FOLDER/group_vars/etcd.yml || error_message "Failed to configure etcd."
 
   cat >> inventory/$CLUSTER_FOLDER/group_vars/all/containerd.yml << EOF
 containerd_registries:
@@ -35,4 +37,6 @@ containerd_registries:
     - "https://mirror.gcr.io"
     - "https://registry-1.docker.io"
 EOF
+
+  success_message "Kubespray configuration completed successfully."
 }
