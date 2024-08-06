bd459f21480da49172948a6f54b06147ab2872b1
hojat-gazestani <hojatgazestani@gmail.com>
2024-08-06 08:41:43 +0330

    fix

--- script/configs/kube_proxy_strict_arp.sh
+++ script/configs/kube_proxy_strict_arp.sh
@@ -4,6 +4,6 @@ source ./prompts/messages.sh
 
 configure_kube_proxy_strict_arp() {
   if [ "$kube_proxy_strict_arp" = true ]; then
-    sed -i 's/kube_proxy_strict_arp: false/kube_proxy_strict_arp: true/' "k8s_config_file" || error_message "Failed to configure kube-proxy strict ARP."
+    sed -i 's/kube_proxy_strict_arp: false/kube_proxy_strict_arp: true/' "$k8s_config_file" || error_message "Failed to configure kube-proxy strict ARP."
   fi
 }
