8f915cbd95f369b371b0fe461e21472257ff5024
hojat-gazestani <hojatgazestani@gmail.com>
2024-08-06 08:37:04 +0330

    arp

--- script/configs/kube_proxy_mode.sh
+++ script/configs/kube_proxy_mode.sh
@@ -4,7 +4,7 @@ source ./prompts/messages.sh
 
 configure_kube_proxy_mode() {
   # Update kube_proxy_mode
-  if [ "$kube_proxy_mode" = "iiptables" ]; then
+  if [ "$kube_proxy_mode" = "iptables" ]; then
     sed -i 's/kube_proxy_mode: ipvs/kube_proxy_mode: iptables/' "$config_file" || error_message "Failed to configure kube-proxy mode."
   fi
 }
--- script/configs/kube_proxy_strict_arp.sh
+++ script/configs/kube_proxy_strict_arp.sh
@@ -3,7 +3,7 @@
 source ./prompts/messages.sh
 
 configure_kube_proxy_strict_arp() {
-  if [ "$kube_proxy_strict_arp" = false ]; then
-    sed -i 's/kube_proxy_strict_arp: true/kube_proxy_strict_arp: false/' "$config_file" || error_message "Failed to configure kube-proxy strict ARP."
+  if [ "$kube_proxy_strict_arp" = true ]; then
+    sed -i 's/kube_proxy_strict_arp: false/kube_proxy_strict_arp: true/' "$config_file" || error_message "Failed to configure kube-proxy strict ARP."
   fi
 }
